38ded64275e54f92e2f53074498451ae73fed000
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-07-15 17:45:42 +0530

    [ADD] servertools_module_remover: AFC-2563 Added servertools_module_remover module to add server action which remove uninstalled module from server.

---
+++ servertools_module_remover/models/modules.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+from odoo.exceptions import Warning
+from odoo import models, fields, _
+
+class Modules(models.Model):
+    _inherit = "ir.module.module"
+
+    def find(self, name, path):
+        for root, dirs, files in os.walk(path):
+            if root.split('/')[-1] == name:
+                return root
+
+    def remove_selected_module_from_server(self):
+        dir_path = self.env['ir.config_parameter'].sudo().get_param('path_to_remove_module_from_server')
+        if not dir_path:
+            raise Warning(_(
+                'Please configure path of parent directory in System Parameter with'
+                ' path_to_remove_module_from_server key!'))
+        module_obj = self.env['ir.module.module']
+        for module in self:
+            if module.state == 'uninstalled':
+                path = self.find(module.name, dir_path)
+                try:
+                    shutil.rmtree(path)
+                    module.unlink()
+                    module_obj.update_list()
+                except Exception as e:
+                    raise Warning("Could not Find module in server!")
+
