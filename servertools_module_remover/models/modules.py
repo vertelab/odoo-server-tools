a0c40bfbb292dd757dc24d80409266c9d136853a
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-09-03 11:31:32 +0530

    [IMP] Improved code.

--- servertools_module_remover/models/modules.py
+++ servertools_module_remover/models/modules.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-from odoo.exceptions import Warning
+from odoo.exceptions import UserError
 from odoo import models, fields, _
 
 class Modules(models.Model):
@@ -15,17 +15,17 @@ class Modules(models.Model):
     def remove_selected_module_from_server(self):
         dir_path = self.env['ir.config_parameter'].sudo().get_param('path_to_remove_module_from_server')
         if not dir_path:
-            raise Warning(_(
+            raise UserError(_(
                 'Please configure path of parent directory in System Parameter with'
                 ' path_to_remove_module_from_server key!'))
         module_obj = self.env['ir.module.module']
         for module in self:
-            if module.state == 'uninstalled':
+            if module.state in ('uninstalled', 'uninstallable'):
                 path = self.find(module.name, dir_path)
                 try:
                     shutil.rmtree(path)
                     module.unlink()
                     module_obj.update_list()
                 except Exception as e:
-                    raise Warning("Could not Find module in server!")
+                    raise UserError("Could not Find module in server!")
 
