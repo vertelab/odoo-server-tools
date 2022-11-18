7b94986abc76b24adb66ced5dc35e9a3702652f0
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-18 14:45:37 +0100

    Update __manifest__.py

--- letsencrypt_nginx/__manifest__.py
+++ letsencrypt_nginx/__manifest__.py
@@ -1,13 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
-    "name": "Let's encrypt nginx",
-    "version": "14.0.1.0.0",
-    "author": "Vertel AB",
-    "license": "AGPL-3",
-    "category": "Hidden/Dependency",
-    "summary": "Create nginx configs for SSL.",
+    'name': 'Server Tools: Let''s Encrypt Nginx',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Create nginx configs for SSL.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Technical',
+    'description': """
+    Create Nginx configs for SSL.
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-server-tools/letsencrypt_nginx',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-server-tools',
+    # Any module necessary for this one to work correctly
     "depends": ["letsencrypt",],
     "data": ["data/ir_config_parameter.xml"],
     "installable": True,
     "external_dependencies": {'python3' : ['acme_tiny', 'IPy']},
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
