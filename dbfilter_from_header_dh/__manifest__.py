d04e80e72c482b9a3738ec14c2be354232579fbd
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-02-08 09:40:04 +0100

    Update __manifest__.py

--- dbfilter_from_header_dh/__manifest__.py
+++ dbfilter_from_header_dh/__manifest__.py
@@ -40,7 +40,7 @@
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-server-tools',
     # Any module necessary for this one to work correctly
-    "depends": ["web"],
+    'depends': ['web'],
     "auto_install": False,
     "installable": True,
 }
