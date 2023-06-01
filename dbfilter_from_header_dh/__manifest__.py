95253a9ba856832284a61dfe6ef96b7ed7bbb45c
iammiracle <miracleaayodele@gmail.com>
2023-06-01 16:35:15 +0200

    migrate dbfilter_from_header

---
+++ dbfilter_from_header_dh/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2013  Therp BV
+# © 2014  ACSONE SA/N
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "dbfilter_from_header_dh",
+    "summary": "Filter databases with HTTP headers with working %d, %h patterns.",
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/vertelab/odoo-server-tools",
+    "author": "Vertel AB, Therp BV, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "depends": ["web"],
+    "auto_install": False,
+    "installable": True,
+}
